b4fbf58950f9648a928720ae61481b8ea7c9c7b1
Guja Babunashvili <Guja1501@gmail.com>
2019-07-01 01:56:35 +0400

    feat: drop in bottle animation

--- fruits-beta/models/fruit.js
+++ fruits-beta/models/fruit.js
@@ -1,83 +1,86 @@
 function Fruit(options) {
-    options = options || {};
-    this.x = options.x || 0;
-    this.y = options.y || 0;
-    this.type = options.type || 'undifined';
-    this.width = options.width || 50;
-    this.height = options.height || 50;
-    this.fallSpeed = options.fallSpeed || 1;
-    this.animationEnd = false;
-    var randomFruitMode = round(random(0, fruitImages[this.type].length - 1));
+  options = options || {};
+  this.x = options.x || 0;
+  this.y = options.y || 0;
+  this.type = options.type || 'undifined';
+  this.width = options.width || 50;
+  this.height = options.height || 50;
+  this.fallSpeed = options.fallSpeed || 1;
+  this.animationEnd = false;
+  var randomFruitMode = round(random(0, fruitImages[this.type].length - 1));
 
-    var scaleCoefficient = 1;
-    var scaleDownSpeed = 0.1;
-    var activeAnimation = null;
+  var scaleCoefficient = 1;
+  var scaleDownSpeed = 0.1;
+  var activeAnimation = null;
 
-    var animations = {
-      scaleDown: {
-        setup: function() {
-          scaleCoefficient = 1;
-        },
-        update: function() {
-          scaleCoefficient = Math.max(scaleCoefficient - scaleDownSpeed, 0);
+  var animations = {
+    scaleDown: {
+      setup: function () {
+        scaleCoefficient = 1;
+      },
+      update: function () {
+        scaleCoefficient = Math.max(scaleCoefficient - scaleDownSpeed, 0);
+        if (scaleCoefficient === 0) {
+          this.animate(null);
         }
-      }
-    };
+      }.bind(this)
+    }
+  };
 
-    this.events = {};
+  this.events = {};
 
-    for(var key in animations) {
-        this.events[key] = {};
+  for (var key in animations) {
+    this.events[key] = {};
+  }
+
+  this.draw = function () {
+    push();
+
+    imageMode(CENTER);
+    image(fruitImages[this.type][randomFruitMode], this.x, this.y, this.width * scaleCoefficient, this.height * scaleCoefficient);
+
+    pop();
+  }
+
+  this.update = function () {
+    updateAnimation();
+    this.y += this.fallSpeed;
+  }
+
+  this.isOutsideOfScene = function () {
+    if (this.y > height + this.height) {
+      return true;
     }
 
-    this.draw = function() {
-        push();
+    return false;
+  }
 
-        imageMode(CENTER);
-        image(fruitImages[this.type][randomFruitMode], this.x, this.y, this.width * scaleCoefficient, this.height * scaleCoefficient);
-
-        pop();
+  this.animate = function (type) {
+    if (activeAnimation) {
+      triggerAnimationEvent(activeAnimation, 'end');
     }
 
-    this.update = function() {
-        updateAnimation();
-        this.y += this.fallSpeed;
+    if (animations.hasOwnProperty(type)) {
+      activeAnimation = type;
+      animations[activeAnimation].setup();
+      triggerAnimationEvent(activeAnimation, 'start');
+    } else {
+      activeAnimation = null;
+    }
+  }
+
+  function updateAnimation() {
+    if (activeAnimation === null) {
+      return;
+    }
+    animations[activeAnimation].update();
+  }
+
+  var triggerAnimationEvent = function (animation, event) {
+    if (this.events.hasOwnProperty(animation) && this.events[animation].hasOwnProperty(event)) {
+      return this.events[animation][event]();
     }
 
-    this.isOutsideOfScene = function() {
-        if(this.y > height + this.height) {
-            return true;
-        }
-
-        return false;
-    }
-
-    this.animate = function(type) {
-      if(activeAnimation) {
-        triggerAnimationEvent(activeAnimation, 'end');
-      }
-
-      if(animations.hasOwnProperty(type)) {
-        activeAnimation = type;
-        animations[activeAnimation].setup();
-        triggerAnimationEvent(activeAnimation, 'start');
-      } else {
-        activeAnimation = null;
-      }
-    } 
-
-    function updateAnimation() {
-      if(activeAnimation === null) {
-        return;
-      }
-      animations[activeAnimation].update();
-    }
-
-    var triggerAnimationEvent = function(animation, event) {
-      if(this.events.hasOwnProperty(animation) && this.events[animation].hasOwnProperty(event)) {
-        return this.events[animation][event]();
-      }
-
-      return false;
-    }.bind(this);
+    return false;
+  }.bind(this);
 }
