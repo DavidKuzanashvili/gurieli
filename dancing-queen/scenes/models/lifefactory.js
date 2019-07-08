5d0438ff602d9fbc92a89b31adc4f9d24acb33b5
Guja Babunashvili <Guja1501@gmail.com>
2019-07-08 17:49:12 +0400

    fix: heart is turn into inactive

--- dancing-queen/scenes/models/lifefactory.js
+++ dancing-queen/scenes/models/lifefactory.js
@@ -5,6 +5,8 @@ function LifeFactory(type,lifeNumber, w, h, gap) {
   this.gap = gap;
   this.lifeNumber = lifeNumber;
   this.lifes = [];
+  this.inactiveLifes = [];
+
   var drawStart = (width / 4) - (this.lifeNumber * (this.w + this.gap) / 2);
 
   this.draw = function() {
@@ -16,12 +18,19 @@ function LifeFactory(type,lifeNumber, w, h, gap) {
   }
 
   this.generetaLifes = function() {
-    for(var i = 0; i < lifeNumber; i++) {
-      this.lifes.push(new Life(type, drawStart + i*(this.w + this.gap), this.w, this.h));
+    while(this.lifes.length < this.lifeNumber) {
+      this.lifes.push(new Life(lifeImages.active, drawStart + this.lifes.length*(this.w + this.gap) + this.gap, this.w, this.h));
+    }
+
+    while(this.inactiveLifes.length < this.lifeNumber) {
+      this.inactiveLifes.push(new Life(lifeImages.inactive, drawStart + this.inactiveLifes.length*(this.w + this.gap) + this.gap, this.w, this.h));
     }
   }
 
   function drawLifes() {
+    oLifeFactory.inactiveLifes.forEach(function(life) {
+      life.draw();
+    });
     oLifeFactory.lifes.forEach(function(life) {
       life.draw();
     });
